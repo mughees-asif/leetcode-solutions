2ab474e986b9acd6450595cf1d82de56a54d7bf0
mughees-asif <mughees460@gmail.com>
2020-07-04 19:22:50 +0100

    isPalindrome

---
+++ Interviews/Strings/ValidPalindrome/src/com/mughees/Main.java
@@ -0,0 +1,12 @@
+package com.mughees;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+    }
+
+    public static boolean isPalindrome(String s) {
+        
+    }
+}
