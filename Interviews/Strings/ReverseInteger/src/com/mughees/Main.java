920b3440545b57cffbb469f65ea567e580979034
mughees-asif <mughees460@gmail.com>
2020-07-04 15:25:27 +0100

    reverseInteger completed

--- Interviews/Strings/ReverseInteger/src/com/mughees/Main.java
+++ Interviews/Strings/ReverseInteger/src/com/mughees/Main.java
@@ -3,12 +3,33 @@ package com.mughees;
 public class Main {
 
     public static void main(String[] args) {
-//        reverseInteger(1234);
-        System.out.println(reverseInteger2(1234));
+        System.out.println(reverse(1234));
+        System.out.println(reverse(-1234));
+        System.out.println(reverse(1534236469));
     }
 
-    static void reverseInteger(int num) {
-        char[] arr = String.valueOf(num).toCharArray();
+    private static int reverse(int num) {
+        int result;
+        char[] arr;
+        try {
+            if (num >= 0) {     // positive numbers
+                arr = String.valueOf(num).toCharArray();
+                reverseInt(arr);
+                result = Integer.parseInt(new String(arr));
+                return result;
+            } else {      // negative numbers
+                num *= -1;
+                arr = String.valueOf(num).toCharArray();
+                reverseInt(arr);
+                result = Integer.parseInt(new String(arr));
+                return -result;
+            }
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+
+    private static void reverseInt(char[] arr) {
         int start = 0;
         int end = arr.length - 1;
         while (start < end) {
@@ -16,18 +37,5 @@ public class Main {
             arr[start++] = arr[end];
             arr[end--] = temp;
         }
-        System.out.println(arr);
-    }
-
-    static int reverseInteger2(int num) {
-        char[] arr = String.valueOf(num).toCharArray();
-        int start = 0;
-        int end = arr.length - 1;
-        while (start < end) {
-            char temp = arr[start];
-            arr[start++] = arr[end];
-            arr[end--] = temp;
-        }
-        return Integer.parseInt(new String(arr));
     }
 }
