748719743d2a3761dbe9902ba562b53119ce7f75
mughees-asif <mughees460@gmail.com>
2020-07-04 12:24:09 +0100

    RotateArray completed

--- Interviews/Arrays/RotateArray/src/com/mughees/Main.java
+++ Interviews/Arrays/RotateArray/src/com/mughees/Main.java
@@ -1,8 +1,28 @@
 package com.mughees;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(Arrays.toString(rotateArray(new int[]{1, 2, 3, 4, 5, 6, 7}, 3)));
+    }
+
+    static int[] rotateArray(int[] nums, int k) {
+        k = k % nums.length;
+        int count = 0;
+        for (int start = 0; count < nums.length; start++) {
+            int current = start;
+            int prev = nums[start];
+            do {
+                int next = (current + k) % nums.length;
+                int temp = nums[next];
+                nums[next] = prev;
+                prev = temp;
+                current = next;
+                count++;
+            } while (start != current);
+        }
+        return nums;
     }
 }
