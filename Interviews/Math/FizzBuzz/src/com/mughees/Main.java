6bdbdabaefb5e85d859c3e1740d639e35ac4ca1c
mughees-asif <mughees460@gmail.com>
2020-07-05 12:31:57 +0100

    FizzBuzz

--- Interviews/Math/FizzBuzz/src/com/mughees/Main.java
+++ Interviews/Math/FizzBuzz/src/com/mughees/Main.java
@@ -1,8 +1,29 @@
 package com.mughees;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+    }
+
+    public List<String> fizzBuzz(int n) {
+        n = Integer.parseInt(String.valueOf(n));
+        List<Integer> list = new ArrayList<>(n);
+        for(int i = 0; i <= n; i++) {
+            if(n % 15 == 0) {
+                System.out.println("FizzBuzz");
+                list.add(n);
+            } else if(n % 3 == 0) {
+                System.out.println("Fizz");
+                list.add(n);
+            } else if(n % 5 == 0) {
+                System.out.println("Buzz");
+                list.add(n);
+            }
+        }
+        return list;
     }
 }
