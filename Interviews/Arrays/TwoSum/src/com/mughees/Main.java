38eafb41ec41748c7c1b023e5348276cd852cd2f
mughees-asif <mughees460@gmail.com>
2020-07-06 11:51:22 +0100

    TwoSum

--- Interviews/Arrays/TwoSum/src/com/mughees/Main.java
+++ Interviews/Arrays/TwoSum/src/com/mughees/Main.java
@@ -1,12 +1,22 @@
 package com.mughees;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9)));
     }
 
-    public int[] twoSum(int[] nums, int target) {
-
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> result = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            int value = target - nums[i];
+            if(result.containsKey(value)) {
+                return new int[] {result.get(value), i};
+            }
+            result.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
     }
 }
