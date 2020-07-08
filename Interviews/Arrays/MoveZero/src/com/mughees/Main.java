710bd2551dd938cf0b30dabd52280a3a370f8d4d
mughees-asif <mughees460@gmail.com>
2020-07-08 15:54:10 +0100

    MoveZero

--- Interviews/Arrays/MoveZero/src/com/mughees/Main.java
+++ Interviews/Arrays/MoveZero/src/com/mughees/Main.java
@@ -1,8 +1,23 @@
 package com.mughees;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(Arrays.toString(moveZeroes(new int[]{0, 1, 0, 3, 12})));
+    }
+
+    public static int[] moveZeroes(int[] nums) {
+        int lastIndex = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[lastIndex++] = nums[i];
+            }
+        }
+        for (int i = lastIndex; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+        return nums;
     }
 }
