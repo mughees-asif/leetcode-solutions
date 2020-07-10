5e45f10dac13490f236bdd205e01ffcfe79dfb05
mughees-asif <mughees460@gmail.com>
2020-07-10 12:12:35 +0100

    Climbing The Stairs completed

--- Interviews/DynamicProgamming/ClimbingTheStairs/src/com/mughees/Main.java
+++ Interviews/DynamicProgamming/ClimbingTheStairs/src/com/mughees/Main.java
@@ -3,6 +3,33 @@ package com.mughees;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(climbStairs(10));
+    }
+
+    public static int climbStairs(int n) {
+        return climb_Stairs(0, n);
+    }
+
+    public static int climb_Stairs(int i, int n) {
+        if (i > n) {
+            return 0;
+        }
+        if (i == n) {
+            return 1;
+        }
+        return climb_Stairs(i + 1, n) + climb_Stairs(i + 2, n);
+    }
+
+    public int climbStairs2(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 }
