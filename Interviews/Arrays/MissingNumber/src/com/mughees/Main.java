bdb4e8b9f239b37008bf87e37bbd570d5cc6c4c7
mughees-asif <mughees460@gmail.com>
2020-07-11 15:29:10 +0100

    MissingNumber completed

--- Interviews/Arrays/MissingNumber/src/com/mughees/Main.java
+++ Interviews/Arrays/MissingNumber/src/com/mughees/Main.java
@@ -1,8 +1,22 @@
 package com.mughees;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(missingNumber(new int[] {3,2,0}));
+        System.out.println(missingNumber(new int[] {3,0,1}));
+        System.out.println(missingNumber(new int[] {9,6,4,2,3,5,7,0,1}));
+    }
+
+    public static int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        int[] tempArr = new int[nums.length + 1];
+        for(int i = nums[0]; i < tempArr.length; i++) {
+            tempArr[i] = i;
+        }
+        Arrays.sort(tempArr);
+        return Arrays.mismatch(tempArr, nums);
     }
 }
