5e115f5d6db80d5276013daf8d77f815c3f2b064
mughees-asif <mughees460@gmail.com>
2020-07-17 15:46:07 +0100

    UniqueCharacter completed

--- Interviews/Strings/UniqueCharacter/src/com/mughees/Main.java
+++ Interviews/Strings/UniqueCharacter/src/com/mughees/Main.java
@@ -1,12 +1,24 @@
 package com.mughees;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String s = "leetcode";
+        System.out.println(firstUniqChar(s));
+
+        String s2 = "loveleetcode";
+        System.out.println(firstUniqChar(s2));
     }
 
-    public int firstUniqChar(String s) {
-
+    public static int firstUniqChar(String s) {
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if (s.indexOf(arr[i]) == s.lastIndexOf(arr[i])) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
