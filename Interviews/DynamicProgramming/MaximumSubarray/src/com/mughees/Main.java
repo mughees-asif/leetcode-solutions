9c637beec7ec2b5f5fd36f9889920a68231e8e80
mughees-asif <mughees460@gmail.com>
2020-07-19 15:44:12 +0100

    MaximumSubarray: completed

--- Interviews/DynamicProgramming/MaximumSubarray/src/com/mughees/Main.java
+++ Interviews/DynamicProgramming/MaximumSubarray/src/com/mughees/Main.java
@@ -3,6 +3,20 @@ package com.mughees;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+    }
+
+    // using Kadane's Algorithm
+    public static int maxSubArray(int[] nums) {
+        int maxSoFar = Integer.MIN_VALUE, maxEndingHere = 0;
+
+        for (int num : nums) {
+            maxEndingHere = maxEndingHere + num;
+            if (maxSoFar < maxEndingHere)
+                maxSoFar = maxEndingHere;
+            if (maxEndingHere < 0)
+                maxEndingHere = 0;
+        }
+        return maxSoFar;
     }
 }
