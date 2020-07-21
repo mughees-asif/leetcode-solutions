72671878bc8811c1aeebce1f44edcba5bdbd4791
mughees-asif <mughees460@gmail.com>
2020-07-21 15:12:28 +0100

    DP: House Robber completed

--- Interviews/DynamicProgramming/HouseRobber/src/com/mughees/Main.java
+++ Interviews/DynamicProgramming/HouseRobber/src/com/mughees/Main.java
@@ -3,15 +3,42 @@ package com.mughees;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(rob(new int[] {1,2,3,1}));
-        System.out.println(rob(new int[] {2,7,9,3,1}));
+        System.out.println(rob(new int[]{1, 2, 3, 1}));
+        System.out.println(rob(new int[]{2, 7, 9, 3, 1}));
+        System.out.println(rob(new int[]{1, 2}));
+
     }
 
     public static int rob(int[] nums) {
-        int sum = 0;
-        for(int i = 0; i < nums.length; i+=2) {
-            sum += nums[i];
+        if (nums == null || nums.length == 0) return 0;
+        if (nums.length == 2 || nums.length == 1) return nums[0];
+
+        int sum = nums[0];
+        int i = 1;
+        while (i < nums.length) {
+            if (i % 2 != 0) {
+                sum += nums[i];
+            }
+            i += 2;
         }
         return sum;
+    }
+
+    public int rob2(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+
+        if (nums.length == 1)
+            return nums[0];
+
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+
+        return dp[nums.length - 1];
     }
 }
