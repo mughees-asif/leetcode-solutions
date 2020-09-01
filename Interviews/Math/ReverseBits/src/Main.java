9a26898d3cce4c99f8bc99549ed2019ec1264e88
mughees-asif <mughees460@gmail.com>
2020-09-01 14:57:47 +0100

    MATH: Reverse Bits completed

--- Interviews/Math/ReverseBits/src/Main.java
+++ Interviews/Math/ReverseBits/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
-    public static void main(String... args) {
+    public static void main(String... args) {}
 
+    public static int reverseBits(int n) {
+        int ans = 0;
+        for (int i = 0; i < 32; i++) {
+            ans <<= 1;
+            ans = ans | (n & 1);
+            n >>= 1;
+        }
+        return ans;
     }
 }
