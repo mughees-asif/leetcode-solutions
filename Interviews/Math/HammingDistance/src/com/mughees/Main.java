4f84e59165e2b027816099dcba732b568dc1005f
mughees-asif <mughees460@gmail.com>
2020-09-04 20:39:42 +0100

    MATH: Hamming Distance completed

--- Interviews/Math/HammingDistance/src/com/mughees/Main.java
+++ Interviews/Math/HammingDistance/src/com/mughees/Main.java
@@ -3,6 +3,16 @@ package com.mughees;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(hammingDistance(93, 73));
+    }
+
+    static int hammingDistance(int x, int y) {
+        int XOR = x ^ y;
+        int setBits = 0;
+        while (XOR > 0) {
+            setBits += XOR & 1;
+            XOR >>= 1;
+        }
+        return setBits;
     }
 }
