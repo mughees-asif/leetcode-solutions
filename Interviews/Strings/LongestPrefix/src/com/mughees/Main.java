a0f52f307c314e53a08e3049fa74de267e01e77a
mughees-asif <mughees460@gmail.com>
2020-09-04 20:07:16 +0100

    STRINGS: Longest Common Prefix

--- Interviews/Strings/LongestPrefix/src/com/mughees/Main.java
+++ Interviews/Strings/LongestPrefix/src/com/mughees/Main.java
@@ -1,8 +1,28 @@
 package com.mughees;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String[] arr = {"flower", "flow", "flight"};
+        System.out.println(longestCommonPrefix(arr));
+    }
+
+    static String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+        if (strs.length == 1) return strs[0];
+
+        Arrays.sort(strs);
+        String firstElement = strs[0];
+        String lastElement = strs[strs.length - 1];
+        // find the minimum length of prefix in the range (first to the last string element in array)
+        int minimumLengthOfPrefix = Math.min(firstElement.length(), lastElement.length());
+
+        int i = 0;
+        while (i < minimumLengthOfPrefix && firstElement.charAt(i) == lastElement.charAt(i)) {
+            i++;
+        }
+        return strs[0].substring(0, i);
     }
 }
