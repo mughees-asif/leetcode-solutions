ea6823e3e6d92e976a48d5577f8834ebf2ef7ce0
mughees-asif <mughees460@gmail.com>
2020-09-18 22:05:40 +0100

    README: Medium Collection started

--- Interviews/Arrays/3Sum/src/com/mughees/Main.java
+++ Interviews/Arrays/3Sum/src/com/mughees/Main.java
@@ -10,18 +10,21 @@ public class Main {
 
     public static List<List<Integer>> threeSum(int[] num) {
         Arrays.sort(num);
+
         List<List<Integer>> res = new LinkedList<>();
-        for (int i = 0; i < num.length-2; i++) {
-            if (i == 0 || (i > 0 && num[i] != num[i-1])) {
-                int lo = i+1, hi = num.length-1, sum = 0 - num[i];
-                while (lo < hi) {
-                    if (num[lo] + num[hi] == sum) {
-                        res.add(Arrays.asList(num[i], num[lo], num[hi]));
-                        while (lo < hi && num[lo] == num[lo+1]) lo++;
-                        while (lo < hi && num[hi] == num[hi-1]) hi--;
-                        lo++; hi--;
-                    } else if (num[lo] + num[hi] < sum) lo++;
-                    else hi--;
+        for (int i = 0; i < num.length - 2; i++) {
+            if (num[i] == 0) break;
+            if (i == 0 || num[i] != num[i - 1]) {
+                int left = i + 1, right = num.length - 1, sum = 0 - num[i];
+                while (left < right) {
+                    if (num[left] + num[right] == sum) {
+                        res.add(Arrays.asList(num[i], num[left], num[right]));
+                        while (left < right && num[left] == num[left + 1]) left++;
+                        while (left < right && num[right] == num[right - 1]) right--;
+                        left++;
+                        right--;
+                    } else if (num[left] + num[right] < sum) left++;
+                    else right--;
                 }
             }
         }
