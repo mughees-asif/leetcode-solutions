b5b1597dcbe4521be31eb2dca679605f403dfdbe
mughees-asif <mughees460@gmail.com>
2020-09-18 22:00:53 +0100

    README: Medium Collection started

--- Interviews/Arrays/3Sum/src/com/mughees/Main.java
+++ Interviews/Arrays/3Sum/src/com/mughees/Main.java
@@ -1,30 +1,30 @@
 package com.mughees;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(threeSum(new int[] {-1,0,1,2,-1,-4}));
+        System.out.println(threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
     }
 
-    public static List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> overallResult = new ArrayList<>();
-        List<Integer> result = new ArrayList<>();
-        int length = nums.length;
-        for (int i = 0; i < length; i++) {
-            for (int j = i + 1; j < length - 1; j++) {
-                for (int k = j + 1; k < length - 2; k++) {
-                    if(nums[i] + nums[j] + nums[k] == 0) {
-                        result.add(nums[i]);
-                        result.add(nums[j]);
-                        result.add(nums[k]);
-                    }
+    public static List<List<Integer>> threeSum(int[] num) {
+        Arrays.sort(num);
+        List<List<Integer>> res = new LinkedList<>();
+        for (int i = 0; i < num.length-2; i++) {
+            if (i == 0 || (i > 0 && num[i] != num[i-1])) {
+                int lo = i+1, hi = num.length-1, sum = 0 - num[i];
+                while (lo < hi) {
+                    if (num[lo] + num[hi] == sum) {
+                        res.add(Arrays.asList(num[i], num[lo], num[hi]));
+                        while (lo < hi && num[lo] == num[lo+1]) lo++;
+                        while (lo < hi && num[hi] == num[hi-1]) hi--;
+                        lo++; hi--;
+                    } else if (num[lo] + num[hi] < sum) lo++;
+                    else hi--;
                 }
             }
-            overallResult.add(result);
         }
-        return overallResult;
+        return res;
     }
 }
