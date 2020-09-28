84271e54190b97d7c3788e1f232a550aaa766809
mughees-asif <mughees460@gmail.com>
2020-09-28 18:45:10 +0100

    STRINGS: Longest substring without repeating characters

--- Interviews/Strings/LongestSubstring/src/com/mughees/Main.java
+++ Interviews/Strings/LongestSubstring/src/com/mughees/Main.java
@@ -1,14 +1,24 @@
 package com.mughees;
 
-import java.util.List;
+import java.util.HashSet;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println(lengthOfLongestSubstring(new String("abcabcbb")));
+        System.out.println(lengthOfLongestSubstring(new String("bbbbb")));
+        System.out.println(lengthOfLongestSubstring(new String("pwwkew")));
     }
 
-    public int lengthOfLongestSubstring(String s) {
-        
+    public static int lengthOfLongestSubstring(String s) {
+        s = s.replaceAll("\\W", "").toLowerCase();
+
+        HashSet<Character> hashSet = new HashSet<>();
+        for(char i = 0; i < s.length();i++) {
+            hashSet.add(s.charAt(i));
+        }
+        System.out.println(hashSet);
+
+        return hashSet.size();
     }
 }
