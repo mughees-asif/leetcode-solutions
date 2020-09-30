e758a2e0237cdffd8760c07c2acbe4b529a4cbf7
mughees-asif <mughees460@gmail.com>
2020-09-30 19:57:30 +0100

    ARRAYS: Spiral Matrix

--- Interviews/Arrays/SpiralMatrix/src/com/mughees/Main.java
+++ Interviews/Arrays/SpiralMatrix/src/com/mughees/Main.java
@@ -1,8 +1,34 @@
 package com.mughees;
 
+import org.w3c.dom.css.CSSImportRule;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[][] test = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        System.out.println(spiralOrder(test));
+
+    }
+
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+
+        for(int row = 0; row < matrix.length; row++) {
+            for(int column = 0; column < matrix[i].length; column++;)
+        }
+
+
+
+        return result;
     }
 }
