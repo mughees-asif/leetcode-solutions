1cb1c027936931267b9a9d3742c140886173864e
mughees-asif <mughees460@gmail.com>
2020-10-02 18:42:26 +0100

    STRINGS: String Compression

--- Interviews/Strings/StringCompression/src/com/mughees/Main.java
+++ Interviews/Strings/StringCompression/src/com/mughees/Main.java
@@ -3,6 +3,9 @@ package com.mughees;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(firstUnique("leetcode"));
+        System.out.println(firstUnique("loveleetcode"));
     }
+
+
 }
