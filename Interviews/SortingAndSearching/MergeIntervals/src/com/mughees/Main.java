abdbc3860497246fbf8a522f05bd912bf958ff75
mughees-asif <mughees460@gmail.com>
2020-10-05 17:39:02 +0100

    SORTING: Merge Intervals

---
+++ Interviews/SortingAndSearching/MergeIntervals/src/com/mughees/Main.java
@@ -0,0 +1,12 @@
+package com.mughees;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+
+    public static int[][] merge(int[][] intervals) {
+        
+    }
+}
