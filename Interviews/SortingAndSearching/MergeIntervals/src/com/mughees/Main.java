4ae5926cc1979f128f990784c4bcab0adf56f571
mughees-asif <mughees460@gmail.com>
2020-10-05 18:25:16 +0100

    SORTING: Merge Intervals

--- Interviews/SortingAndSearching/MergeIntervals/src/com/mughees/Main.java
+++ Interviews/SortingAndSearching/MergeIntervals/src/com/mughees/Main.java
@@ -1,12 +1,38 @@
 package com.mughees;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        int[][] arr = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+        int[][] arr2 = {{1, 4}, {4, 5}};
+        System.out.println(Arrays.deepToString(merge(arr)));
+        System.out.println(Arrays.deepToString(merge(arr2)));
     }
 
     public static int[][] merge(int[][] intervals) {
-        
+        List<List<Integer>> list = new ArrayList<>();
+        for (int outer = 0; outer < intervals.length; outer++) {
+            for (int inner = 1; inner < intervals[outer].length; inner++) {
+                if (intervals[outer][inner] >= intervals[outer + 1][inner - 1]) {
+                    list.add(merge1(intervals[outer], intervals[outer + 1]));
+                } else {
+                    return intervals;
+                }
+            }
+        }
+        return list.toArray(intervals);
+    }
+
+    static List<Integer> merge1(int[] a, int[] b) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0, j = b.length - 1; i < a.length; i++) {
+            result.add(a[i]);
+            result.add(b[j]);
+        }
+        return result;
     }
 }
