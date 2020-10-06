f57e701c5435b10ba24a1882d14f3ee902f044f2
mughees-asif <mughees460@gmail.com>
2020-10-06 20:52:43 +0100

    MATH: Median of Two Sorted Arrays

--- Interviews/Math/MedianSortedArrays/src/com/mughees/Main.java
+++ Interviews/Math/MedianSortedArrays/src/com/mughees/Main.java
@@ -1,9 +1,6 @@
 package com.mughees;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class Main {
 
@@ -12,13 +9,18 @@ public class Main {
         System.out.println(findMedianSortedArrays(new int[]{0, 0}, new int[]{0, 0}));
         System.out.println(findMedianSortedArrays(new int[]{}, new int[]{1}));
         System.out.println(findMedianSortedArrays(new int[]{2}, new int[]{0}));
+        System.out.println(findMedianSortedArrays(new int[]{1, 3}, new int[]{2}));
     }
 
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int[] result = new int[nums1.length + nums2.length];
         System.arraycopy(nums1, 0, result, 0, nums1.length);
-        System.arraycopy(nums1,0, result, nums1.length, nums2.length);
-        System.out.println(Arrays.toString(result));
-        return 0;
+        System.arraycopy(nums2, 0, result, nums1.length, nums2.length);
+        int n = result.length;
+        Arrays.sort(result);
+        if (n % 2 != 0) {
+            return result[n / 2];
+        }
+        return (result[(n - 1) / 2] + result[n / 2]) / 2.0;
     }
 }
