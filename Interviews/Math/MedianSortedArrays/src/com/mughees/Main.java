58c226b5166eb5a8cc7e8f74f3eb57068cfdf492
mughees-asif <mughees460@gmail.com>
2020-10-06 21:02:43 +0100

    MATH: Median of Two Sorted Arrays

--- Interviews/Math/MedianSortedArrays/src/com/mughees/Main.java
+++ Interviews/Math/MedianSortedArrays/src/com/mughees/Main.java
@@ -10,17 +10,22 @@ public class Main {
         System.out.println(findMedianSortedArrays(new int[]{}, new int[]{1}));
         System.out.println(findMedianSortedArrays(new int[]{2}, new int[]{0}));
         System.out.println(findMedianSortedArrays(new int[]{1, 3}, new int[]{2}));
+        System.out.println(findMedianSortedArrays(new int[]{1, 2, 3}, new int[]{4, 5, 6}));
     }
 
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int[] result = new int[nums1.length + nums2.length];
         System.arraycopy(nums1, 0, result, 0, nums1.length);
         System.arraycopy(nums2, 0, result, nums1.length, nums2.length);
-        int n = result.length;
+        int length = result.length;
         Arrays.sort(result);
-        if (n % 2 != 0) {
-            return result[n / 2];
+
+        // Median of a sorted array of size n is defined as the middle element
+        // when length is odd and average of middle two elements when n is even.
+
+        if (length % 2 != 0) {
+            return result[length / 2];
         }
-        return (result[(n - 1) / 2] + result[n / 2]) / 2.0;
+        return (result[(length - 1) / 2] + result[length / 2]) / 2.0;
     }
 }
