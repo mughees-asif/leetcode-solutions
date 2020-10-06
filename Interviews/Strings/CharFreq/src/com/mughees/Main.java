50908dc84181291c9e748d17bad9a9a0da14b7f8
mughees-asif <mughees460@gmail.com>
2020-10-06 19:21:59 +0100

    STRINGS: Sort Character by Frequency

--- Interviews/Strings/CharFreq/src/com/mughees/Main.java
+++ Interviews/Strings/CharFreq/src/com/mughees/Main.java
@@ -1,8 +1,26 @@
 package com.mughees;
 
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        frequencySort("tree");
+        frequencySort("cccaaa");
+        frequencySort("Aabb");
+
+    }
+
+    public static void frequencySort(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(char c : s.toCharArray()) {
+            if(map.containsKey(c)) {
+                map.put(c, map.get(c) + 1);
+            } else {
+                map.put(c, 1);
+            }
+        }
+        map.forEach((K, V) -> System.out.print(K + " " + V + " "));
     }
 }
