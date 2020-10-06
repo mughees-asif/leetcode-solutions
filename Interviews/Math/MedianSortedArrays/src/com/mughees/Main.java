87c1e4755d211a4e271d0fbb9289fa2d8f397a99
mughees-asif <mughees460@gmail.com>
2020-10-06 20:25:58 +0100

    MATH: Median of Two Sorted Arrays

--- Interviews/Math/MedianSortedArrays/src/com/mughees/Main.java
+++ Interviews/Math/MedianSortedArrays/src/com/mughees/Main.java
@@ -1,8 +1,23 @@
 package com.mughees;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
     }
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        double median = 0;
+        List<Integer> list = new ArrayList<>();
+        int length = nums1.length + nums2.length;
+        
+        for(int i = 0; i < nums1.length; i++) {
+
+        }
+
+    }
+
 }
