80676c482df4e5efb3e697b81ad48d33fccdc320
mughees-asif <mughees460@gmail.com>
2020-10-09 15:20:22 +0100

    SEARCHING: Top K Frequent Words

--- Interviews/SortingAndSearching/TopKWords/src/com/mughees/Main.java
+++ Interviews/SortingAndSearching/TopKWords/src/com/mughees/Main.java
@@ -17,21 +17,24 @@ public class Main {
             map.put(s, map.getOrDefault(s, 0) + 1);
         }
 
-        // check
+        // check for alphabetical order
         PriorityQueue<String> pq = new PriorityQueue<>((o1, o2) -> {
             int freq_1 = map.get(o1);
             int freq_2 = map.get(o2);
-            if(freq_1 < freq_2) return o2.compareTo(o1);
+            if(freq_1 == freq_2) return o2.compareTo(o1);
             return freq_1 - freq_2;
         });
 
+        // remove all elements that are more the 'K' frequency
         for(Map.Entry<String, Integer> entry : map.entrySet()) {
             pq.add(entry.getKey());
             if(pq.size() > k)   pq.poll();
         }
 
-        Collections.reverse(result);
+        // reverse the result list, as `.poll()` method
+        // returns the element at the front of the queue
         while(!pq.isEmpty())    result.add(pq.poll());
+        Collections.reverse(result);
         return result;
     }
 }
