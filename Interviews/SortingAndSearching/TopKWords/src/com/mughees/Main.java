a570530e2152ee52377dae493d5e5cb0742b0e9e
mughees-asif <mughees460@gmail.com>
2020-10-09 14:48:22 +0100

    SEARCHING: Top K Frequent Words

--- Interviews/SortingAndSearching/TopKWords/src/com/mughees/Main.java
+++ Interviews/SortingAndSearching/TopKWords/src/com/mughees/Main.java
@@ -1,8 +1,24 @@
 package com.mughees;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(topKFrequent(new String[]{"i", "love", "leetcode", "i", "love", "coding"}, 2));
+    }
+
+    public static List<String> topKFrequent(String[] words, int k) {
+        List<String> list = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+        for (String s : words) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+
+        System.out.println(map);
+        return list;
     }
 }
