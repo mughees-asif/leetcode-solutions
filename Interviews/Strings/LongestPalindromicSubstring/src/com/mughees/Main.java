fc5f08fcd7f10f20ec89ae714c90684124e27482
mughees-asif <mughees460@gmail.com>
2020-10-09 13:58:23 +0100

    STRINGS: Longest Palindromic Substring

--- Interviews/Strings/LongestPalindromicSubstring/src/com/mughees/Main.java
+++ Interviews/Strings/LongestPalindromicSubstring/src/com/mughees/Main.java
@@ -3,6 +3,34 @@ package com.mughees;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+    }
+
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+
+        int left = 0;
+        int right = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int length_1 = expandFromMiddle(s, i, i);
+            int length_2 = expandFromMiddle(s, i, i + 1);
+            int length = Math.max(length_1, length_2);
+
+            if(length > left - right) {
+
+            }
+
+        }
+    }
+
+    public static int expandFromMiddle(String s, int start, int end) {
+        if (s == null || start > end) return 0;
+
+        while (start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)) {
+            start--;
+            end++;
+        }
+        return end - start + 1;
     }
 }
