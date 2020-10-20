b0059fb405814b57a23b8a5c12b30969d7d8f5d8
mughees-asif <mughees460@gmail.com>
2020-10-20 19:07:28 +0100

    ARRAYS_MEDIUM: Container With Most Water

--- Interviews/Arrays/ContainerWithMostWater/src/com/mughees/Main.java
+++ Interviews/Arrays/ContainerWithMostWater/src/com/mughees/Main.java
@@ -3,6 +3,24 @@ package com.mughees;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+    }
+
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            // check to see which is the smaller side of the container
+            if (height[left] < height[right]) {
+                maxArea = Math.max(maxArea, height[left] * (right - left));
+                left++;
+            } else {
+                maxArea = Math.max(maxArea, height[right] * (right - left));
+                right--;
+            }
+        }
+        return maxArea;
     }
 }
