6f86e027fa70a2c960ffafa0cd503dd4194710d7
mughees-asif <mughees460@gmail.com>
2020-10-20 17:09:53 +0100

    ARRAYS_MEDIUM: Kth Largest Element in Array

--- Interviews/Arrays/KthLargestElement/src/com/mughees/Main.java
+++ Interviews/Arrays/KthLargestElement/src/com/mughees/Main.java
@@ -1,8 +1,25 @@
 package com.mughees;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(findKthLargest(new int[] {3,2,3,1,2,4,5,5,6}, 4));
     }
+    public static int findKthLargest(int[] nums, int k) {
+        int result = 0;
+        Arrays.sort(nums);
+        // reverse sorted array - descending
+        for(int i = nums.length - 1, j = 0; i > 0; i--, j++) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+        System.out.println(Arrays.toString(nums));
+
+
+        return result;
+    }
+
 }
