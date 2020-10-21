a5a55afa0813791b5e85fdc0ccc0a3b62251b05b
mughees-asif <mughees460@gmail.com>
2020-10-21 15:59:53 +0100

    STRINGS_MEDIUM: Find And Replace

--- Interviews/Strings/FindAndReplace/src/com/mughees/Main.java
+++ Interviews/Strings/FindAndReplace/src/com/mughees/Main.java
@@ -3,6 +3,29 @@ package com.mughees;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(findReplaceString("abcd", new int[]{0, 2}, new String[]{"a", "cd"}, new String[]{"ee", "ffff"}));
+    }
+
+    public static String findReplaceString(String S, int[] indexes, String[] sources, String[] targets) {
+        int stringPointer = 0;
+        int indexPointer = 0;
+        int sourcesPointer = 0;
+        int targetsPointer = 0;
+
+        StringBuilder result = new StringBuilder();
+        char[] stringArray = S.toCharArray();
+
+        for (int i = 1; i < indexes.length; i++) {
+
+            if (S.substring(0, indexes[i]).equals(sources[i])) {
+                result.append(targets[i]);
+                System.out.println(result);
+            }
+
+
+        }
+
+
+        return result.toString();
     }
 }
