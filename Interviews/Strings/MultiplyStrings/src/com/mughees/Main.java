c6c72af27a67b1a5aa353e67dc58d0222f3d8149
mughees-asif <mughees460@gmail.com>
2020-10-29 15:12:42 +0000

    MEDIUM_STRINGS: Multiply Strings

--- Interviews/Strings/MultiplyStrings/src/com/mughees/Main.java
+++ Interviews/Strings/MultiplyStrings/src/com/mughees/Main.java
@@ -13,15 +13,15 @@ public class Main {
         for(int i = length_1 - 1; i >= 0; i--) { // iterate over first number
             for(int j = length_2 - 1; j >= 0; j--) { // iterate over second number
                 int multiplication = (num1.charAt(i) - '0') * (num2.charAt(j) - '0'); // multiply both digits
-                int sum = resultArray[i + j + 1] + multiplication; //
-                resultArray[i + j] += sum / 10; //
+                int sum = resultArray[i + j + 1] + multiplication; // saves space by doing addition in the same array
+                resultArray[i + j] += sum / 10; // the number to be carried over is put in the position right before
                 resultArray[i + j + 1] += sum % 10; // get the last digit
             }
         }
 
         StringBuilder sb = new StringBuilder();
         for(int result : resultArray) {
-            if(sb.length() != 0 || result != 0) { // check to make sure no leading zeros
+            if(sb.length() != 0 || result != 0) { // check to eliminate leading zeros
                 sb.append(result);
             }
         }
