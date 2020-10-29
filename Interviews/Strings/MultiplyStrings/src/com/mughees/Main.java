7e6eeedad82346c530ad0e254256da6abc7a6002
mughees-asif <mughees460@gmail.com>
2020-10-29 14:06:27 +0000

    MEDIUM_STRINGS: Multiply Strings

--- Interviews/Strings/MultiplyStrings/src/com/mughees/Main.java
+++ Interviews/Strings/MultiplyStrings/src/com/mughees/Main.java
@@ -7,16 +7,23 @@ public class Main {
     }
 
     public static String multiply(String num1, String num2) {
-        int s1 = Integer.parseInt(num1);
-        int s2 = Integer.parseInt(num2);
-        int product = s1 * s2;
-        return String.valueOf(product);
+        int s1 = converter(num1);
+        int s2 = converter(num2);
+        return String.valueOf(s1 * s2);
+    }
+
+    private static int converter(String num1) {
+        int num = 0;
+        int i = 0;
+        while( i < num1.length()) {
+            num *= 10;
+            num += num1.charAt(i++) - '0'; // Minus the ASCII code of '0' to get the value of the charAt(i++).
+        }
+        return num;
     }
 
 
 
-//        while( i < str.length()) {
-//        num *= 10;
-//        num += str.charAt(i++) - '0'; // Minus the ASCII code of '0' to get the value of the charAt(i++).
-//    }
+
+
 }
