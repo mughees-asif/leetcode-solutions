3676c2d1b50026e8c056e71095796fded950bc82
mughees-asif <mughees460@gmail.com>
2020-10-29 14:32:34 +0000

    MEDIUM_STRINGS: Multiply Strings

--- Interviews/Strings/MultiplyStrings/src/com/mughees/Main.java
+++ Interviews/Strings/MultiplyStrings/src/com/mughees/Main.java
@@ -7,23 +7,76 @@ public class Main {
     }
 
     public static String multiply(String num1, String num2) {
-        int s1 = converter(num1);
-        int s2 = converter(num2);
-        return String.valueOf(s1 * s2);
-    }
 
-    private static int converter(String num1) {
-        int num = 0;
-        int i = 0;
-        while( i < num1.length()) {
-            num *= 10;
-            num += num1.charAt(i++) - '0'; // Minus the ASCII code of '0' to get the value of the charAt(i++).
+        int len1 = num1.length();
+        int len2 = num2.length();
+        if (len1 == 0 || len2 == 0)
+            return "0";
+
+        // will keep the result number in vector
+        // in reverse order
+        int[] result = new int[len1 + len2];
+
+        // Below two indexes are used to
+        // find positions in result.
+        int i_n1 = 0;
+        int i_n2;
+
+        // Go from right to left in num1
+        for (int i = len1 - 1; i >= 0; i--)
+        {
+            int carry = 0;
+            int n1 = num1.charAt(i) - '0';
+
+            // To shift position to left after every
+            // multipliccharAtion of a digit in num2
+            i_n2 = 0;
+
+            // Go from right to left in num2
+            for (int j = len2 - 1; j >= 0; j--)
+            {
+                // Take current digit of second number
+                int n2 = num2.charAt(j) - '0';
+
+                // Multiply with current digit of first number
+                // and add result to previously stored result
+                // charAt current position.
+                int sum = n1 * n2 + result[i_n1 + i_n2] + carry;
+
+                // Carry for next itercharAtion
+                carry = sum / 10;
+
+                // Store result
+                result[i_n1 + i_n2] = sum % 10;
+
+                i_n2++;
+            }
+
+            // store carry in next cell
+            if (carry > 0)
+                result[i_n1 + i_n2] += carry;
+
+            // To shift position to left after every
+            // multipliccharAtion of a digit in num1.
+            i_n1++;
         }
-        return num;
+
+        // ignore '0's from the right
+        int i = result.length - 1;
+        while (i >= 0 && result[i] == 0)
+            i--;
+
+        // If all were '0's - means either both
+        // or one of num1 or num2 were '0'
+        if (i == -1)
+            return "0";
+
+        // genercharAte the result String
+        StringBuilder s = new StringBuilder();
+
+        while (i >= 0)
+            s.append(result[i--]);
+
+        return s.toString();
     }
-
-
-
-
-
 }
