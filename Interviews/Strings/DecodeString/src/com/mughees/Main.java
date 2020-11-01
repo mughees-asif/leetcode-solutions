5a4cd68e6cc82769123b77f1cf2907b2d49bfd84
mughees-asif <mughees460@gmail.com>
2020-11-01 13:04:58 +0000

    MEDIUM_STRINGS: Decode String

--- Interviews/Strings/DecodeString/src/com/mughees/Main.java
+++ Interviews/Strings/DecodeString/src/com/mughees/Main.java
@@ -5,16 +5,28 @@ import java.util.Stack;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
     }
 
     public String decodeString(String s) {
         int index = 0;
+        String result = "";
         Stack<Integer> count = new Stack<>();
         Stack<Character> ch = new Stack<>();
 
-        
+        while (index < s.length()) {
+            // first condition: digit
 
 
+            // second condition: open bracket ('[')
+
+
+            //third condition: close bracket (']')
+
+
+            // fourth condition: character
+
+        }
+        return result;
     }
 }
