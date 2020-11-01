e5cea17bec498401881e0cea193f6ed718657fcc
mughees-asif <mughees460@gmail.com>
2020-11-01 13:02:16 +0000

    MEDIUM_STRINGS: Decode String

--- Interviews/Strings/DecodeString/src/com/mughees/Main.java
+++ Interviews/Strings/DecodeString/src/com/mughees/Main.java
@@ -1,8 +1,20 @@
 package com.mughees;
 
+import java.util.Stack;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+    }
+
+    public String decodeString(String s) {
+        int index = 0;
+        Stack<Integer> count = new Stack<>();
+        Stack<Character> ch = new Stack<>();
+
+        
+
+
     }
 }
