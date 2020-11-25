9dd4ecd7ff90d901f052768c6f037ba0cd41f64e
mughees-asif <mughees460@gmail.com>
2020-11-25 19:26:31 +0000

    MEDIUM_STRINGS: Backspace String Comparison

--- Interviews/Strings/BackspaceStringComparison/src/com/mughees/Main.java
+++ Interviews/Strings/BackspaceStringComparison/src/com/mughees/Main.java
@@ -6,10 +6,12 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-	    
+        System.out.println(backspaceCompare("ab#c", "ad#c"));
+        System.out.println(backspaceCompare("ab##", "c#d#"));
+        System.out.println(backspaceCompare("a##c", "a#c#"));
     }
 
-    public boolean backspaceCompare(String S, String T) {
+    public static boolean backspaceCompare(String S, String T) {
         List<Character> first = new ArrayList<>();
         for(char c : S.toCharArray()) {
             first.add(c);
@@ -22,11 +24,21 @@ public class Main {
 
         for (int i = 0; i < first.size(); i++) {
             if(first.get(i) == '#') {
+                first.remove(i);
                 first.remove(i -1);
             }
         }
 
-        return true;
-    }
+        for (int i = 0; i < second.size(); i++) {
+            if (second.get(i) == '#') {
+                second.remove(i);
+                second.remove(i - 1);
+            }
+        }
 
+        System.out.println(first);
+        System.out.println(second);
+
+        return first.equals(second);
+    }
 }
