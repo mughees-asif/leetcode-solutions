5918f75250052bc640eeaabb237bc6c2c4c0c57c
mughees-asif <mughees460@gmail.com>
2020-12-01 16:15:04 +0000

    MEDIUM_SORTING&SEARCHING: Merge Two Sorted Lists

---
+++ Interviews/SortingAndSearching/MergeKSortedLists/src/com/mughees/Main.java
@@ -0,0 +1,19 @@
+package com.mughees;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
