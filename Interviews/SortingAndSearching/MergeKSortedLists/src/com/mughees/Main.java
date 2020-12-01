1dff1848e169f302ccf3de1ae34950dee023487c
mughees-asif <mughees460@gmail.com>
2020-12-01 16:22:58 +0000

    MEDIUM_SORTING&SEARCHING: Merge Two Sorted Lists

--- Interviews/SortingAndSearching/MergeKSortedLists/src/com/mughees/Main.java
+++ Interviews/SortingAndSearching/MergeKSortedLists/src/com/mughees/Main.java
@@ -1,19 +1,39 @@
 package com.mughees;
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+import java.util.*;
+
+//Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
+
+    public static ListNode mergeTwoLists(ListNode list_1, ListNode list_2) {
+        ListNode result = null;
+
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+
+        return result;
+    }
+
 }
