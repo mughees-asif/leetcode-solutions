e5df143a136dfe6b65b89b88eff62b13ab4340c8
mughees-asif <mughees460@gmail.com>
2020-12-16 15:48:58 +0000

    ARRAYS_MEDIUM: Product of array except self

--- Interviews/Arrays/ProductOfArrayExceptSelf/src/com/mughees/Main.java
+++ Interviews/Arrays/ProductOfArrayExceptSelf/src/com/mughees/Main.java
@@ -1,8 +1,19 @@
 package com.mughees;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(Arrays.toString(productExceptSelf(new int[]{1, 2, 3, 4})));
+    }
+
+    public static int[] productExceptSelf(int[] nums) {
+        int[] output = new int[nums.length];
+        
+
+
+
+        return output;
     }
 }
