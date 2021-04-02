fcbb0dd0322cc4e20980b17f04f27de6bb54d423
mughees-asif <mughees460@gmail.com>
2021-04-02 18:41:17 +0100

    FirstAndLastNumberLocation

--- Interviews/SortingAndSearching/src/com/mughees/Main.java
+++ Interviews/SortingAndSearching/src/com/mughees/Main.java
@@ -1,8 +1,48 @@
 package com.mughees;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(Arrays.toString(searchRange(new int[]{5, 7, 7, 8, 8, 10}, 8)));
+        System.out.println(Arrays.toString(searchRange(new int[]{5, 7, 7, 8, 8, 10}, 6)));
+        System.out.println(Arrays.toString(searchRange(new int[]{}, 0)));
+    }
+
+    public static int[] searchRange(int[] nums, int target) {
+        int firstOccurrence = findBound(nums, target, true);
+        if (firstOccurrence == -1) {
+            return new int[]{-1, -1};
+        }
+        int lastOccurrence = findBound(nums, target, false);
+        return new int[]{firstOccurrence, lastOccurrence};
+    }
+
+    private static int findBound(int[] nums, int target, boolean isFirst) {
+        int N = nums.length;
+        int begin = 0, end = N - 1;
+
+        while (begin <= end) {
+            int mid = begin + (end - begin) / 2;
+            if (nums[mid] == target) {
+                if (isFirst) {
+                    if (mid == begin || nums[mid - 1] != target) {
+                        return mid;
+                    }
+                    end = mid - 1;
+                } else {
+                    if (mid == end || nums[mid + 1] != target) {
+                        return mid;
+                    }
+                    begin = mid + 1;
+                }
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                begin = mid + 1;
+            }
+        }
+        return -1;
     }
 }
